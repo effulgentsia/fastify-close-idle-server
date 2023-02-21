5ffbe33dec89e070375514e00594b3df7a0b45a3
effulgentsia <alex.bronstein@acquia.com>
2023-02-21 06:12:49 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+
+const fastifyIdleServerShutdown = fp(function (fastify, opts, done) {
+  const delay = opts.delay || 60000
+  const close = opts.closeFunction || fastify.close.bind(fastify)
+
+  let timer = null
+  function startIdleTimer() {
+    if (!timer) {
+      timer = setTimeout(idleTimeLimitReached, delay)
+      timer.unref()
+      fastify.log.info('Idle timer started')
+    }
+  }
+  function stopIdleTimer() {
+    if (timer) {
+      clearTimeout(timer)
+      timer = null
+      fastify.log.info('Idle timer stopped')
+    }
+  }
+  function idleTimeLimitReached() {
+    fastify.log.info('Idle time limit reached')
+    close()
+  }
+  fastify.addHook('onReady', (done) => {
+    startIdleTimer()
+    done()
+  })
+
+  let numActiveRequests = 0
+  function onRequestStarted() {
+    stopIdleTimer()
+    numActiveRequests++
+  }
+  function onRequestFinished() {
+    numActiveRequests--
+    if (numActiveRequests === 0) {
+      startIdleTimer()
+    }
+  }
+  fastify.addHook('onRequest', (request, reply, done) => {
+    onRequestStarted()
+    reply.then(onRequestFinished, onRequestFinished)
+    done()
+  })
+
+  done()
+})
+
+module.exports = fastifyIdleServerShutdown
+module.exports.default = fastifyIdleServerShutdown
+module.exports.fastifyIdleServerShutdown = fastifyIdleServerShutdown
